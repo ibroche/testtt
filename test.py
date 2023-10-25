3a306a30eb3400c7e1da9d38ed90a0ffd22f89b1
ibroche <HE304488@students.ephec.be>
2023-10-25 16:19:38 +0200

    add code in test

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-
+from machine import Pin
+import time
+led = Pin(25, Pin.OUT) #Built-in LED Pico
+#led = Pin("LED", Pin.OUT) #Built-in LED Pico W
+try:
+    while True:
+        led.value(1) #Set led turn on
+        time.sleep(0.5) #Sleep 0.5s
+        led.value(0) # Setled turn off
+        time.sleep(0.5) #Sleep 0.5s
+except:
+    pass
